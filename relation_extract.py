45d96d23043245fdb1cb94150269e932c429ad75
Ronny Worm <r-worm@gmx.de>
2015-12-22 00:46:59 +0100

    Tabellen werden bereits mit Inhalt befüllt

--- relation_extract.py
+++ relation_extract.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
 import sqlite3
+import os
+
+os.remove("relations.db")
 
 def read_reverb_relations(filename):
-	result = []
+	subjects = []
+	verbs = []
+	objects = []
 
 	with open (filename, "r") as f:
 		content = f.read()
@@ -11,17 +16,23 @@ def read_reverb_relations(filename):
 		for line in content.split("\n"):
 			splitted = line.split(";")
 			if len(splitted) == 18:
-				result += [ line.split(";")[16].replace(" ", "_") ]
+				subjects += [ line.split(";")[15] ]
+				verbs += [ line.split(";")[16].replace(" ", "_") ]
+				objects += [ line.split(";")[17] ]
 
-	return result
+	return [ subjects, verbs, objects ]
 
 def create_tables(relations):
 	conn = sqlite3.connect('relations.db')
 	c = conn.cursor()
 
+	index = 0
+	for table in relations[1]:
 
-	for rel in relations:
-		c.execute("CREATE TABLE %s (subject text, object text)" % rel)
+		c.execute("CREATE TABLE %s (subject text, object text)" % table) 
+		c.execute("INSERT INTO %s VALUES ('%s','%s')" % (table, relations[0][index], relations[2][index]))
+
+		index += 1	
 
 	conn.close()
 
