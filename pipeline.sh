804977464ad4404a62a7ede91ce03e46de4d0d05
Ronny Worm <r-worm@gmx.de>
2015-12-30 20:09:47 +0100

    Umwandlung von pipeline in ein QA-System (mit Main-Loop)

--- pipeline.sh
+++ pipeline.sh
@@ -12,16 +12,19 @@ qfile="question_normalised.txt"
 #q="How many tables are in this room?" # -> keine Tabellen
 
 
+# Parameterhandling
 if [ $# -eq 0 ]; then
 	cat << EOF
 Frage-Antwort-System - Prototyp
 
 Als erster Parameter kann dbready übergeben werden, wenn die Datenbank bereits erzeugt wurde, ansonsten wird sie neu erstellt.
-Als zweiter Parameter kann eine Frage übergeben werden, ansonsten wird die Frage zur Laufzeit aufgenommen.
+Als zweiter Parameter kann eine erste Frage übergeben werden, ansonsten wird die erste Frage zur Laufzeit aufgenommen.
+Als dritter Parameter kann justonce übergeben werden, damit nur eine einzige Frage bearbeitet wird.
 
 EOF
-
 fi
+
+
 
 if [[ $1 == "dbready" ]]; then
 	if [ $debug -eq 1 ]; then
@@ -36,34 +39,51 @@ else
 fi	
 
 
-if [[ $# -ne 2 || -z "$2" ]]; then
-    echo -n "Ask a question about the document(s) in '$corpus': "
-    read q
-else
-	q="$2"
-fi
-./process_question.sh "$q"
-res=$?
-if [ $res -eq 1 ]; then
-	echo "Your question could not be processed, sorry."
-	# Hier könnte ich nochmal versuchen, die Frage weiterzuverarbeiten - vll mit dem Stanford-Parser
-	exit
-fi
+if [[ $3 == "justonce" ]]; then
+	justonce="true"
+fi	
 
-question_verb="$(awk 'NR == 2' $qfile)"
-syns=$(./get_synonyms.py "$question_verb" v 2)
 
-wait
 
-tables=$(./get_matching_table_names.py $db "$question_verb" "$syns")
-
-if [ ! -z "$tables" ]; then
-	answers=$(./print_matches_in_tables.py $db $qfile "$tables")
-	if [ -z "$answers" ]; then
-		echo "Unfortunately there is no information about your question."	
+qasystem(){
+	if [[ $# -lt 2 || -z "$2" ]]; then
+	    echo -n "Ask a question about the document(s) in '$corpus': "
+	    read q
 	else
-		echo "$answers"
+		q="$2"
 	fi
-else
-	echo "Sorry, I don't know the answer."
+	./process_question.sh "$q"
+	res=$?
+	if [ $res -eq 1 ]; then
+		echo "Your question could not be processed, sorry."
+		# Hier könnte ich nochmal versuchen, die Frage weiterzuverarbeiten - vll mit dem Stanford-Parser
+		return
+	fi
+
+	question_verb="$(awk 'NR == 2' $qfile)"
+	syns=$(./get_synonyms.py "$question_verb" v 2)
+
+	# Warte auf process_corpus
+	wait
+
+	tables=$(./get_matching_table_names.py $db "$question_verb" "$syns")
+
+	if [ ! -z "$tables" ]; then
+		answers=$(./print_matches_in_tables.py $db $qfile "$tables")
+		if [ -z "$answers" ]; then
+			echo "Unfortunately there is no information about your question."	
+		else
+			echo "$answers"
+		fi
+	else
+		echo "Sorry, I don't know the answer."
+	fi
+}
+
+
+qasystem "$@"
+if [ -z "$justonce" ]; then
+	while [ 1 ]; do
+	   qasystem
+	done
 fi
