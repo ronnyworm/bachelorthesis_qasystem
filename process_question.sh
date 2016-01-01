2db920d2fc9432fe76faad06f2637861eeeb0392
Ronny Worm <r-worm@gmx.de>
2016-01-01 21:16:29 +0100

    Variable in process_question umbenannt

--- process_question.sh
+++ process_question.sh
@@ -16,17 +16,18 @@ echo "$q" > question.txt
 if [ $debug -eq 1 ]; then
 	echo "Process question ..."
 fi
-java -Xmx512m -jar ReVerb/reverb-latest.jar question.txt 2> /dev/null | awk -v FS="\t" '{printf("%s\n%s\n%s\n", $16, $17, $18)}' > question_normalised.txt 
+java -Xmx512m -jar ReVerb/reverb-latest.jar question.txt 2> /dev/null | awk -v FS="\t" '{printf("%s\n%s\n%s\n", $16, $17, $18)}' > question_relation.txt 
 rm question.txt
 
-file_size_kb=`du -k "question_normalised.txt" | cut -f1`
+#vll lieber mit $() ?
+file_size_kb=`du -k "question_relation.txt" | cut -f1`
 
 
 if [ $file_size_kb -eq 0 ]; then
 	exit 1
 else
 	if [ $debug -eq 1 ]; then
-		cat question_normalised.txt
+		cat question_relation.txt
 	fi
 	exit 0
 fi
